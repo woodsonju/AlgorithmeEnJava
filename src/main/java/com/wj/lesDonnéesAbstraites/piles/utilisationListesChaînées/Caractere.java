a19fb01a162b7ce63d53100df4a4fda820908a0c
Woodson Juste <j.woodson@hotmail.fr>
2019-10-06 12:42:26 +0200

    Implémentation de la classe Caractere.java

---
+++ src/main/java/com/wj/lesDonnéesAbstraites/piles/utilisationListesChaînées/Caractere.java
@@ -0,0 +1,14 @@
+package com.wj.lesDonnéesAbstraites.piles.utilisationListesChaînées;
+
+public class Caractere {
+    private char caractere;
+    
+    public Caractere(char c) {
+        this.caractere = c;
+    }
+    
+    public char getDonnee() {
+        return this.caractere;
+    }
+    
+}
---
+++ src/main/java/com/wj/lesDonnéesAbstraites/piles/utilisationListesChaînées/Cpile.java
@@ -0,0 +1,4 @@
+package com.wj.lesDonnéesAbstraites.piles.utilisationListesChaînées;
+
+public class Cpile {
+}
